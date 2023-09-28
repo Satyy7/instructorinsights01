44cf395408e4461fa84a864c3e1a0e03dccc6566
Faris <fariskarim2000@gmail.com>
2023-09-27 21:40:58 -0400

    selenium start; press show more button and extract professors

---
+++ main.py
@@ -0,0 +1,110 @@
+import requests
+import re
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://www.ratemyprofessors.com"
+
+
+def extract_professor_count(content):
+    match = re.search(r'"resultCount":(\d+)', content)
+    if match:
+        count = match.group(1)
+        return f"{count} professors at Vanderbilt University"
+    return None
+
+
+def search_and_print_header():
+    search_url = f"{BASE_URL}/search/professors/4002?q=*"
+    response = requests.get(search_url)
+
+    if response.status_code != 200:
+        print("Failed to fetch the webpage.")
+        return
+
+    header_info = extract_professor_count(response.text)
+    if header_info:
+        print(header_info)
+    else:
+        print("Header information not found.")
+
+
+def extract_professor_data(soup):
+    professor_elements = soup.find_all(class_="TeacherCard__StyledTeacherCard-syjs0d-0")
+    
+    professors = []
+    for element in professor_elements:
+        name_element = element.find(class_="CardName__StyledCardName-sc-1gyrgim-0")
+        rating_element = element.find(class_="CardNumRating__CardNumRatingNumber-sc-17t4b9u-2")
+        department_element = element.find(class_="CardSchool__Department-sc-19lmz2k-0")
+
+        if name_element and rating_element and department_element:
+            name = name_element.get_text(strip=True)
+            rating = rating_element.get_text(strip=True)
+            department = department_element.get_text(strip=True)
+            print(f"Name: {name}, Rating: {rating}, Department: {department}")
+
+            professors.append((name, rating, department))
+            
+    return professors
+
+
+def scrape_professors():
+    options = webdriver.ChromeOptions()
+
+    prefs = {
+        "profile.managed_default_content_settings.images": 2,
+        "profile.default_content_setting_values.notifications": 2,
+        "profile.default_content_setting_values.ads": 2,
+        "profile.managed_default_content_settings.stylesheet": 2,
+        "profile.managed_default_content_settings.cookies": 2,
+        "profile.managed_default_content_settings.javascript": 1,
+        "profile.managed_default_content_settings.plugins": 1,
+        "profile.managed_default_content_settings.popups": 2,
+        "profile.managed_default_content_settings.geolocation": 2,
+        "profile.managed_default_content_settings.media_stream": 2,
+    }
+
+    options.add_experimental_option("prefs", prefs)
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-popup-blocking")
+
+    driver = webdriver.Chrome(options=options)
+    url = f"{BASE_URL}/search/professors/4002?q=*"
+    driver.get(url)
+    wait = WebDriverWait(driver, 3)
+
+    try:
+        close_button = wait.until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, ".CCPAModal__StyledCloseButton-sc-10x9kq-2")))
+        close_button.click()
+        print("Button closed")
+    except Exception as e:
+        print(
+            f"Could not find Close button, or another error occurred: {str(e)}")
+    i = 0
+    while i < 5:
+        try:
+            show_more_button = wait.until(EC.visibility_of_element_located(
+                (By.XPATH, "//button[text()='Show More']")))
+            show_more_button.click()
+            print("Clicked Show More Button")
+            
+            i += 1
+        except Exception as e:
+            print("No more 'Show More' buttons, or another error occurred.")
+            break
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    extract_professor_data(soup)
+    driver.quit()
+
+if __name__ == "__main__":
+    search_and_print_header()
+    scrape_professors()
